7ddc20a95dc105d4853f1c54bc428ee48bce3776
Lương Văn Tú <87352056+luongvantuit@users.noreply.github.com>
2021-10-14 19:51:01 +0700

    add module controller view welcome base

--- src/main/java/appx/s/learn/se06/Se06Application.java
+++ src/main/java/appx/s/learn/se06/Se06Application.java
@@ -2,24 +2,13 @@ package appx.s.learn.se06;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
+@SpringBootApplication(scanBasePackages = {"appx.s.learn"})
 public class Se06Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Se06Application.class, args);
     }
 
-    @GetMapping(value = "/")
-    @ResponseBody
-    ResponseEntity<Object> get() {
-        return new ResponseEntity<>("Welcome to API", HttpStatus.OK);
-    }
 
 }
