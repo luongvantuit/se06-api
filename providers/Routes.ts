e175e62d9a0603f4f1693e11822ee4fbb627066c
luongvantuit <luongvantuit@gmail.com>
2021-11-17 18:21:53 +0700

    join path string

--- providers/Routes.ts
+++ providers/Routes.ts
@@ -2,11 +2,12 @@ import { Application } from "express";
 import API from "../routes/API";
 import Web from "../routes/Web";
 import Locals from "./Locals";
+import { join } from 'path'
 
 class Routers {
 
     public mountAPI(_express: Application): Application {
-        return _express.use(Locals.config().APIPrefix, API);
+        return _express.use(join("/", Locals.config().APIPrefix), API);
     }
 
     public mountWeb(_express: Application): Application {
