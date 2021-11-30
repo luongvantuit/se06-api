64994d54e3cf6b81e8ff92f3124ebaac75267a17
luongvantuit <luongvantuit@gmail.com>
2021-11-30 10:29:52 +0700

    api get rate of product /api/rate/:id

--- controllers/Product/RateController.ts
+++ controllers/Product/RateController.ts
@@ -1,7 +1,17 @@
+import { ParamsDictionary } from "express-serve-static-core";
+import { ParsedQs } from "qs";
 import IController from "../../interfaces/vendors/IController";
+import IRequest from "../../interfaces/vendors/IRequest";
+import IResponse from "../../interfaces/vendors/IResponse";
+import Rate from "../../models/Rate";
 
 class RateController extends IController {
 
+    public async index(req: IRequest<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: IResponse<any, Record<string, any>>): Promise<void> {
+        const { id } = req.params;
+        const doc = await Rate.find({ productID: id });
+        res.send(JSON.stringify(doc));
+    }
 }
 
 export default new RateController;
--- routes/API.ts
+++ routes/API.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import RateController from "../controllers/Product/RateController";
 
 const API: Router = Router();
+
+API.get('/rate/:id', RateController.index)
+
 
 export default API;
