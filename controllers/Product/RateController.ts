a02142b20dc9ef1c836675df1e695002929755c4
luongvantuit <luongvantuit@gmail.com>
2021-11-30 10:40:37 +0700

    response data for api rate

--- controllers/Product/RateController.ts
+++ controllers/Product/RateController.ts
@@ -1,5 +1,6 @@
 import { ParamsDictionary } from "express-serve-static-core";
 import { ParsedQs } from "qs";
+import IBaseResponse from "../../interfaces/vendors/IBaseResponse";
 import IController from "../../interfaces/vendors/IController";
 import IRequest from "../../interfaces/vendors/IRequest";
 import IResponse from "../../interfaces/vendors/IResponse";
@@ -7,10 +8,16 @@ import Rate from "../../models/Rate";
 
 class RateController extends IController {
 
-    public async index(req: IRequest<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: IResponse<any, Record<string, any>>): Promise<void> {
+    public async index(req: IRequest<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: IResponse<IBaseResponse<any>, Record<string, any>>): Promise<void> {
         const { id } = req.params;
         const doc = await Rate.find({ productID: id });
-        res.send(JSON.stringify(doc));
+        res.status(200);
+        res.send({
+            error: false,
+            messages: `Rate of product with id: ${id}`,
+            data: doc,
+            code: 200,
+        });
     }
 }
 
