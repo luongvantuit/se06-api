a7c5205e888c06d0f66876ca600fd3f3b6388920
luongvantuit <luongvantuit@gmail.com>
2021-12-08 10:15:23 +0700

    404 ejs

--- routes/Web.ts
+++ routes/Web.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import HomeController from "../controllers/Views/HomeController";
+import HttpStatusCode from "../exception/HttpStatusCode";
 
 const Web: Router = Router();
 
 Web.get("/", HomeController.index);
+
+Web.get("*", (res, req) => {
+    return req.status(HttpStatusCode.NOT_FOUND).render('404.ejs');
+})
 
 export default Web;
