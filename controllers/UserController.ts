39729d9cd57a5291eba4e5f0d7b42cf2010e4788
luongvantuit <luongvantuit@gmail.com>
2021-12-10 10:02:37 +0700

    api get user

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -4,9 +4,23 @@ import IBaseResponse from "../interfaces/vendors/IBaseResponse";
 import IController from "../interfaces/vendors/IController";
 import IRequest from "../interfaces/vendors/IRequest";
 import IResponse from "../interfaces/vendors/IResponse";
+import User from "../models/User";
+import HttpStatusCode from "../perform/HttpStatusCode";
+import Token from "../perform/Token";
 
 class UserController extends IController {
-    public index(req: IRequest<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: IResponse<IBaseResponse<any>, Record<string, any>>): void {
+    public async index(req: IRequest<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: IResponse<IBaseResponse<any>, Record<string, any>>): Promise<void> {
+        return await Token.verify(req, res, async (req, res, auth) => {
+            const user = User.findOne({ uid: auth.uid })
+            return await res.status(HttpStatusCode.OK)
+                .send({
+                    error: false,
+                    data: user,
+                }).end();
+        });
+    }
+
+    public show(req: IRequest<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: IResponse<IBaseResponse<any>, Record<string, any>>): void {
 
     }
 
--- routes/API.ts
+++ routes/API.ts
@@ -1,8 +1,10 @@
 import { Router, json } from "express";
 import CheckEmailRecordController from "../controllers/CheckEmailRecordController";
+import UserController from "../controllers/UserController";
 const API: Router = Router();
 
 API.post('/check/email/record', json(), CheckEmailRecordController.index)
+API.get('/user', UserController.index);
 
 
 export default API;
