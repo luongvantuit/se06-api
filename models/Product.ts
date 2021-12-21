aa1f093cbaa3a723d2f1d1b9e7ee5d9f20390c6d
luongvantuit <luongvantuit@gmail.com>
2021-12-21 10:09:27 +0700

    product model & product controller

--- models/Product.ts
+++ models/Product.ts
@@ -1,6 +1,32 @@
+import { model, Model, Schema } from "mongoose";
+
 interface IProduct {
     description?: string,
     sid: string,
     displayName?: string,
     photos?: string[],
+    address?: string,
 }
+
+interface ProductModel extends IProduct, Document {
+
+}
+
+const ProductSchema = new Schema({
+    description: { type: String },
+    sid: { type: String, },
+    displayName: { type: String },
+    photos: [{ type: String, default: [] }],
+    address: { type: String },
+})
+
+const Product: Model<ProductModel> = model<ProductModel>('product', ProductSchema);
+
+
+export {
+    IProduct,
+    ProductModel,
+    ProductSchema,
+}
+
+export default Product;
--- routes/API.ts
+++ routes/API.ts
@@ -1,6 +1,7 @@
 import { Router, json } from "express";
 import CardController from "../controllers/CardController";
 import CheckEmailRecordController from "../controllers/CheckEmailRecordController";
+import ProductController from "../controllers/ProductController";
 import ShopController from "../controllers/ShopController";
 import UserController from "../controllers/UserController";
 const API: Router = Router();
@@ -14,18 +15,26 @@ API.post('/user', json(), UserController.create);
 API.put('/user', json(), UserController.update);
 
 // All api shop
-API.get('/shop', ShopController.index);
+API.get('/shops', ShopController.index);
 API.get('/shop/:sid', ShopController.show);
 API.post('/shop', json(), ShopController.create);
 API.put('/shop/:sid', json(), ShopController.update);
 API.delete('/shop/:sid', json(), ShopController.destroy);
 
 // API card
-API.get('/card', CardController.index);
+API.get('/cards', CardController.index);
 API.get('/card/:cid', CardController.show);
 API.post('/card', json(), CardController.create);
 API.put('/card/:cid', json(), CardController.update);
 API.delete('/card/:cid', json(), CardController.destroy);
+
+// API product
+API.get('/products/:sid', ProductController.index);
+API.get('/product/:pid', ProductController.show);
+API.post('/product/:sid', json(), ProductController.create);
+API.put('/product/:sid/:pid', json(), ProductController.update);
+API.delete('/product/:sid/:pid', json(), ProductController.destroy);
+
 
 
 export default API;
