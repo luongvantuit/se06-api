3e8ea9cfb131dbde6cb3e123e4c9730f15a7a753
Lương Văn Tú <luongvantuit@gmail.com>
2022-01-18 08:04:05 +0700

    remove resource front end

--- Dockerfile
+++ Dockerfile
@@ -8,16 +8,4 @@ RUN yarn install
 
 RUN cat .env.docker >> .env
 
-# RUN rm -rf ./build
-
-# RUN npx tsc
-
-# RUN cp -R ./resources ./build/resources 
-
-# RUN cat ./serviceAccountKey.json >> ./build/serviceAccountKey.json
-
-# RUN cp /app/api/.env /app/api/build/
-
-# CMD [ "node","./build/index.js" ]
-
 CMD [ "yarn","start" ]
--- providers/Routes.ts
+++ providers/Routes.ts
@@ -1,6 +1,5 @@
 import { Application } from "express";
 import API from "../routes/API";
-import Web from "../routes/Web";
 import Locals from "./Locals";
 import { join } from 'path'
 
@@ -8,10 +7,6 @@ class Routers {
 
     public mountAPI(_express: Application): Application {
         return _express.use(join("/", Locals.config().APIPrefix), API);
-    }
-
-    public mountWeb(_express: Application): Application {
-        return _express.use("/", Web);
     }
 }
 
