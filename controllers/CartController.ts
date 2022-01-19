be3a770ea9b8550760413de80d5191cb6ceb9d99
Lương Văn Tú <luongvantuit@gmail.com>
2022-01-19 09:55:44 +0700

    cart model

--- controllers/CartController.ts
+++ controllers/CartController.ts
@@ -1,7 +1,11 @@
 import IController from "../interfaces/vendors/IController";
+import IRequest from "../interfaces/vendors/IRequest";
+import IResponse from "../interfaces/vendors/IResponse";
 
 class CartController extends IController {
+    public index(req: IRequest, res: IResponse) {
 
+    }
 }
 
 
--- models/Cart.ts
+++ models/Cart.ts
@@ -1,7 +1,9 @@
 import { Document, model, Schema, Model } from 'mongoose';
 
 interface ICart {
-
+    pid: string;
+    quantily: number;
+    classify?: number;
 }
 
 interface CartModel extends ICart, Document {
@@ -9,7 +11,9 @@ interface CartModel extends ICart, Document {
 }
 
 const CartSchema = new Schema({
-
+    pid: { type: String, required: true },
+    quantily: { type: Number, required: true },
+    classify: { type: Number }
 });
 
 const Cart: Model<CartModel> = model<CartModel>('cart', CartSchema);
