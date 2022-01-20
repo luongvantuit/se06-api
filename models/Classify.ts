4b81c026e357596b771e8d272bedf98659485895
Lương Văn Tú <luongvantuit@gmail.com>
2022-01-20 23:37:58 +0700

    classify model

---
+++ models/Classify.ts
@@ -0,0 +1,35 @@
+import { Document, model, Model, Schema } from "mongoose";
+
+interface IClassify {
+    price: number,
+    displayName: string,
+    quantily: number,
+    description: string,
+    uid: string,
+    sid: string,
+}
+
+interface ClassifyModel extends Document, IClassify {
+
+}
+
+const ClassifyShema = new Schema({
+    uid: { type: String, required: true },
+    sid: { type: String, required: true },
+    price: { type: Number, required: true },
+    displayName: { type: String, required: true },
+    quantily: { type: Number, required: true },
+    description: { type: String, required: true }
+})
+
+const Classify: Model<ClassifyModel> = model<ClassifyModel>('classify', ClassifyShema);
+
+
+export default Classify;
+
+export {
+    IClassify,
+    ClassifyModel,
+    ClassifyShema,
+}
+
