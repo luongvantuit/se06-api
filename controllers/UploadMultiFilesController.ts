48440c83b2bcd4918ca467e007e6bba4b4bc422e
Lương Văn Tú <luongvantuit@gmail.com>
2022-01-21 10:47:16 +0700

    fix response upload multi files

--- controllers/UploadMultiFilesController.ts
+++ controllers/UploadMultiFilesController.ts
@@ -1,6 +1,7 @@
 import IController from "../interfaces/vendors/IController";
 import IRequest from "../interfaces/vendors/IRequest";
 import IResponse from "../interfaces/vendors/IResponse";
+import Log from "../middlewares/Log";
 import HttpStatusCode from "../perform/HttpStatusCode";
 import Token from "../perform/Token";
 import { uploadMultiFiles } from "../services/Multer";
@@ -12,18 +13,33 @@ class UploadMultiFilesController extends IController {
      * @param res 
      */
     public async create(req: IRequest, res: IResponse) {
-        await Token.verify(req, res, (req, res) => {
-            uploadMultiFiles(req, res, (error) => {
+        await Token.verify(req, res, async (req, res) => {
+            uploadMultiFiles(req, res, async (error) => {
                 if (error) {
-                    res.status(HttpStatusCode.OK).send({
+                    const response: any = {
                         error: true,
-                    });
+                        status: HttpStatusCode.BAD_REQUEST,
+                        path: req.path,
+                        method: req.method,
+                        msg: 'upload failed!',
+                        data: {
+                            error: error
+                        }
+                    }
+                    Log.default(response);
+                    await res.status(HttpStatusCode.BAD_REQUEST).send(response);
                 }
                 else {
-                    res.status(HttpStatusCode.OK).send({
+                    const response: any = {
                         error: false,
-                        data: req.files
-                    });
+                        data: req.files,
+                        status: HttpStatusCode.OK,
+                        path: req.path,
+                        method: req.method,
+                        msg: 'upload success!'
+                    }
+                    Log.default(response);
+                    await res.status(HttpStatusCode.OK).send(response);
                 }
             });
         });
